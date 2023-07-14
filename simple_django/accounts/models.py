883d127eb3662295170bcbe497cad61b55c2e75e
Josh Karamuth <hello@joshkaramuth.com>
2023-07-14 14:47:02 +0400

    Add method to update login timestamp

--- simple_django/accounts/models.py
+++ simple_django/accounts/models.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 
 class User(AbstractUser):
+    def update_login_timestamp(self):
+        self.last_login = timezone.now()
+        self.save()
+
     class Meta:
         ordering = ["-date_joined"]
         db_table = "users"
